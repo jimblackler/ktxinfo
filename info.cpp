2f3dbbfa38e6021086cc40017be56849b45a23cb
Jim Blackler <jimblackler@google.com>
2019-06-18 11:40:31 +0100

    Reformat to column width.

--- info.cpp
+++ info.cpp
@@ -30,20 +30,28 @@ void output_header(ktxTexture* texture) {
                       {VariadicTableType::AUTO, VariadicTableJustify::RIGHT},
                       {VariadicTableType::AUTO, VariadicTableJustify::AUTO}});
 
-  vt.addRow({"glFormat", texture->glFormat, toHex(texture->glFormat), name_from_enum(texture->glFormat)});
-  vt.addRow({"glInternalformat", texture->glInternalformat, toHex(texture->glInternalformat),
+  vt.addRow({"glFormat", texture->glFormat, toHex(texture->glFormat),
+             name_from_enum(texture->glFormat)});
+  vt.addRow({"glInternalformat", texture->glInternalformat,
+             toHex(texture->glInternalformat),
              name_from_enum(texture->glInternalformat)});
-  vt.addRow({"glBaseInternalformat", texture->glBaseInternalformat, toHex(texture->glBaseInternalformat),
+  vt.addRow({"glBaseInternalformat", texture->glBaseInternalformat,
+             toHex(texture->glBaseInternalformat),
              name_from_enum(texture->glBaseInternalformat)});
-  vt.addRow({"glType", texture->glType, toHex(texture->glType), name_from_enum(texture->glType)});
+  vt.addRow({"glType", texture->glType, toHex(texture->glType),
+             name_from_enum(texture->glType)});
   vt.addRow({"isArray", texture->isArray, toHex(texture->isArray), ""});
   vt.addRow({"isCubemap", texture->isCubemap, toHex(texture->isCubemap), ""});
-  vt.addRow({"isCompressed", texture->isCompressed, toHex(texture->isCompressed), ""});
-  vt.addRow({"generateMipmaps", texture->generateMipmaps, toHex(texture->generateMipmaps), ""});
+  vt.addRow({"isCompressed", texture->isCompressed,
+             toHex(texture->isCompressed), ""});
+  vt.addRow({"generateMipmaps", texture->generateMipmaps,
+             toHex(texture->generateMipmaps), ""});
   vt.addRow({"baseWidth", texture->baseWidth, toHex(texture->baseWidth), ""});
-  vt.addRow({"baseHeight", texture->baseHeight, toHex(texture->baseHeight), ""});
+  vt.addRow({"baseHeight", texture->baseHeight, toHex(texture->baseHeight),
+             ""});
   vt.addRow({"baseDepth", texture->baseDepth, toHex(texture->baseDepth), ""});
-  vt.addRow({"numDimensions", texture->numDimensions, toHex(texture->numDimensions), ""});
+  vt.addRow({"numDimensions", texture->numDimensions,
+             toHex(texture->numDimensions), ""});
   vt.addRow({"numLevels", texture->numLevels, toHex(texture->numLevels), ""});
   vt.addRow({"numLayers", texture->numLayers, toHex(texture->numLayers), ""});
   vt.addRow({"numFaces", texture->numFaces, toHex(texture->numFaces), ""});
