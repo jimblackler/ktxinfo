a80ca12974c674bb0b07f0defa08ec6754b86884
Jim Blackler <jimblackler@google.com>
2019-06-18 11:40:00 +0100

    Display key/value metadata.

--- info.cpp
+++ info.cpp
@@ -1,10 +1,19 @@
 extern "C" {
 #include "info.h"
 #include "glnames.h"
+#include "ktx.h"
 }
 
 #include <sstream>
+#include <cstring>
 #include "variadictable.h"
+
+#define PADN(n, nbytes) (nbytes + (n-1) & ~(ktx_uint32_t)(n-1))
+
+/*
+ * Pad nbytes to next multiple of 4
+ */
+#define _KTX_PAD4(nbytes) _KTX_PADN(4, nbytes)
 
 std::string toHex(int i) {
   std::ostringstream stringStream;
@@ -12,9 +21,9 @@ std::string toHex(int i) {
   return stringStream.str();
 }
 
-void output_info(ktxTexture *texture) {
-
-  VariadicTable<std::string, int, std::string, std::string> vt({"Field", "Dec", "Hex", "Enum"});
+void output_header(ktxTexture* texture) {
+  VariadicTable<std::string, int, std::string, std::string>
+      vt({"Field", "Dec", "Hex", "Enum"});
 
   vt.setColumnFormat({{VariadicTableType::AUTO, VariadicTableJustify::AUTO},
                       {VariadicTableType::AUTO, VariadicTableJustify::AUTO},
@@ -42,4 +51,32 @@ void output_info(ktxTexture *texture) {
   vt.addRow({"dataSize", texture->dataSize, toHex(texture->dataSize), ""});
 
   vt.print(std::cout);
+}
+
+
+void output_key_values(ktxTexture* texture) {
+  VariadicTable<std::string, std::string> vt({"Key", "Value"});
+
+  unsigned int kvdLen;
+  unsigned char* pKvd;
+
+  ktxHashList_Serialize(&texture->kvDataHead, &kvdLen, &pKvd);
+  auto src = (char*) pKvd;
+  while (src < (char*) pKvd + kvdLen) {
+    ktx_uint32_t keyAndValueByteSize = *((ktx_uint32_t*) src);
+    src += sizeof(keyAndValueByteSize);
+    char* key = src;
+    unsigned int keyLen = (unsigned int) strlen(key) + 1;
+    char* value = key + keyLen;
+    vt.addRow({key, value});
+    src += PADN(4, keyAndValueByteSize);
+  }
+
+  vt.print(std::cout);
+}
+
+
+void output_info(ktxTexture* texture) {
+  output_header(texture);
+  output_key_values(texture);
 }
